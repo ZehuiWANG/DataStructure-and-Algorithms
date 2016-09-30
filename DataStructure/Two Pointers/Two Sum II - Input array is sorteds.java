dffcd402a515aaffb769ccff6aa2f1165050735a
Zehui <chinazehuiwang@gmail.com>
2016-09-30 16:09:51 -0400

    Update Two Sum II - Input array is sorteds.java

--- DataStructure/Two Pointers/Two Sum II - Input array is sorteds.java
+++ DataStructure/Two Pointers/Two Sum II - Input array is sorteds.java
@@ -47,3 +47,27 @@ public class Solution {
        return null;
     }
 }
+////O(N)
+
+public class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+        int[] result = new int[2];
+        
+        int left = 0;
+        int right = numbers.length - 1;
+        
+        while (left < right) {
+            int v = numbers[left] + numbers[right];
+            if (v == target) {
+                result[0] = left + 1;
+                result[1] = right + 1;
+                return result;
+            } else if (v > target) {
+                right --;
+            } else {
+                left ++;
+            }
+        }
+        return null;
+    }
+}
